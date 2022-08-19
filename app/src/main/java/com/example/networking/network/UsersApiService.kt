aa362d7be991b3af87c81531b85bcb5f2d670ebf
npogoncuk <npogoncuk@gmail.com>
2022-08-19 21:53:33 +0300

    Added HttpLoggingInterceptor

--- app/src/main/java/com/example/networking/network/UsersApiService.kt
+++ app/src/main/java/com/example/networking/network/UsersApiService.kt
@@ -2,6 +2,8 @@ package com.example.networking.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -12,9 +14,13 @@ private const val BASE_URL = "https://randomuser.me"
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
+
+private val client = OkHttpClient.Builder()
+    .addInterceptor(HttpLoggingInterceptor())
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))    //ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
+    .client(client.build())
     .build()
 
 interface ResultsApiService {
